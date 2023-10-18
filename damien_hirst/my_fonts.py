76830b08bac911252ecc273d5949281aef9d13e7
tony hollaar <thollaar@gmail.com>
2023-10-17 23:52:58 -0400

    update app

---
+++ damien_hirst/my_fonts.py
@@ -0,0 +1,63 @@
+import streamlit as st
+
+# SET FONT STYLES
+font_style = """
+            <style>
+            @import url('https://fonts.googleapis.com/css2?family=Ysabeau+SC:wght@200&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Rubik+Dirt&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@200&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
+            @import url('https://fonts.googleapis.com/css2?family=Koulen&family=Oswald&display=swap');
+            /* Set the font family for paragraph elements */
+            p {
+               font-family: 'Ysabeau SC', sans-serif;
+            }
+            
+            /* Set the font family, size, and weight for unordered list and ordered list elements */
+            ul, ol {
+                font-family: 'Ysabeau SC', sans-serif;
+                font-size: 16px;
+                font-weight: normal;
+            }
+            
+            /* Set the font family, size, weight, and margin for list item elements */
+            li {
+                font-family: 'Ysabeau SC', sans-serif;
+                font-size: 16px;
+                font-weight: normal;
+                margin-top: 5px;
+            }
+            </style>
+            """
+
+def my_text_header(my_string,
+                   my_text_align='center', 
+                   my_font_family='Lobster, cursive;',
+                   my_font_weight=200,
+                   my_font_size='36px',
+                   my_line_height=1.5):
+    text_header = f'<h1 style="text-align:{my_text_align}; font-family: {my_font_family}; font-weight: {my_font_weight}; font-size: {my_font_size}; line-height: {my_line_height};">{my_string}</h1>'
+    st.markdown(text_header, unsafe_allow_html=True)
+
+def my_text_paragraph(my_string,
+                        link_url=None,
+                        my_text_align='center',
+                        my_font_family='Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
+                        my_font_weight=200,
+                        my_font_size='18px',
+                        my_line_height=1.5,
+                        add_border=False,
+                        border_color="#45B8AC"):
+    if add_border:
+      border_style = f'border: 2px solid {border_color}; border-radius: 10px; padding: 10px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);'
+    else:
+        border_style = ''
+    
+    if link_url:
+        paragraph = f'<p style="text-align:{my_text_align}; font-family:{my_font_family}; font-weight:{my_font_weight}; font-size:{my_font_size}; line-height:{my_line_height}; background-color: rgba(255, 255, 255, 0); {border_style}">{my_string} <a href="{link_url}" target="_blank">Click here</a></p>'
+    else:
+        paragraph = f'<p style="text-align:{my_text_align}; font-family:{my_font_family}; font-weight:{my_font_weight}; font-size:{my_font_size}; line-height:{my_line_height}; background-color: rgba(255, 255, 255, 0); {border_style}">{my_string}</p>'
+    
+    st.markdown(paragraph, unsafe_allow_html=True)        
--- damien_hirst/streamlit_app.py
+++ damien_hirst/streamlit_app.py
@@ -3,23 +3,22 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from io import BytesIO
+from my_fonts import font_style, my_text_header, my_text_paragraph
+
+st.set_page_config(
+    page_title="Dotty",
+    layout="centered",
+    page_icon="🟣",
+    initial_sidebar_state="expanded"
+)
+
+# SETUP DIFFERENT FONT STYLES
+st.markdown(font_style, unsafe_allow_html=True)
 
 class DotPainter:
-    """
-    DotPainter is a Streamlit application for creating customizable spot paintings inspired by Damien Hirst.
-
-    This class allows users to configure various parameters such as dot size, gap size, canvas size, dot shape, and resolution.
-
-    Methods:
-        create_dot_painting: Generates and displays the spot painting based on user-defined settings.
-        run: Runs the Streamlit application, allowing users to interact with and customize the spot painting.
-    """
     def __init__(self):
-        # Streamlit app title
-        st.title("Create your Spot Painting")
-        st.subheader("Inspiration from Damien Hirst")
-
-        # Define the color palette
+        my_text_header('CREATE YOUR SPOT PAINTING', my_font_family='Oswald')
+        my_text_paragraph("Inspired by Damien Hirst")
         self.color_list = [(239, 240, 243), (50, 90, 148), (235, 151, 89), (207, 10, 51), (146, 66, 99),
                           (232, 208, 90), (237, 69, 105), (59, 174, 71), (124, 182, 164), (60, 137, 110),
                           (117, 166, 209), (249, 67, 24), (199, 145, 168), (241, 248, 244), (117, 89, 65),
@@ -27,9 +26,12 @@ class DotPainter:
                           (1, 85, 116), (178, 199, 179), (64, 60, 56), (76, 47, 54), (40, 163, 210),
                           (44, 35, 89), (218, 175, 187), (231, 172, 163)]
 
-    def create_dot_painting(self, dot_size, gap_size, canvas_size, shape, dpi):
+    def create_dot_painting(self, dot_size, gap_size, canvas_size, shape, dpi, figsize_ratio=1.0, image_format="SVG"):
+        # Calculate the actual figsize using the given ratio
+        figsize = canvas_size / 100 * figsize_ratio
+
         # Create a figure and axis
-        fig, ax = plt.subplots(figsize=(canvas_size / 100, canvas_size / 100))
+        fig, ax = plt.subplots(figsize=(figsize, figsize))
 
         # Set the aspect ratio to be equal so the dots are round
         ax.set_aspect('equal')
@@ -61,35 +63,60 @@ class DotPainter:
                 ax.scatter(x, y, c=[color], s=dot_size, marker=marker)
 
         # Display the dot painting
-        st.pyplot(fig)
+        # st.pyplot(fig)
 
-        # Add a download button to save the figure as an image with the selected DPI
-        buffer = BytesIO()
-        plt.savefig(buffer, format="png", dpi=dpi)
+        if image_format == "SVG":
+            # Create an SVG image
+            buffer = BytesIO()
+            plt.savefig(buffer, format="svg", bbox_inches='tight')  # Use 'tight' to remove excess white space
+            buffer.seek(0)
+
+            svg_content = f'''<div style="text-align: center;">{buffer.getvalue().decode()}</div>'''
+            st.markdown(svg_content, unsafe_allow_html=True)
+        else:
+            # Create a JPEG image
+            buffer = BytesIO()
+            plt.savefig(buffer, format="jpeg", bbox_inches='tight', dpi=dpi)  # Use 'tight' to remove excess white space
+            buffer.seek(0)
+
+            st.image(buffer)
 
         with st.sidebar:
-            st.divider()
-            st.title('Download')
+            my_text_header('download', my_font_family='Oswald')
 
             with st.columns([1, 50, 1])[1]:
-                st.download_button(
-                    label=f"Download Image (DPI={dpi})",
-                    data=buffer,
-                    file_name=f"dot_painting_{dpi}dpi.png",
-                    mime="image/png",
-                    use_container_width=True
-                )
+                if image_format == "SVG":
+                    st.download_button(
+                        label=f"Download Image (SVG)",
+                        data=buffer,
+                        file_name=f"dot_painting.svg",
+                        mime="image/svg+xml",
+                        key="svg_download_button",
+                        use_container_width=True
+                    )
+                else:
+                    st.download_button(
+                        label=f"Download Image (JPEG)",
+                        data=buffer,
+                        file_name=f"dot_painting.jpeg",
+                        mime="image/jpeg",
+                        key="jpeg_download_button",
+                        use_container_width=True
+                    )
 
     def run(self):
-        # Sidebar with user inputs
-        st.sidebar.header("Customization Options")
-        dot_size = st.sidebar.slider("Dot Size", 5, 50, 15)
-        gap_size = st.sidebar.slider("Gap Size", 10, 50, 30)
-        canvas_size = st.sidebar.slider("Canvas Size", 200, 800, 400)
+        with st.sidebar:
+            my_text_header('palette', my_font_family='Oswald')
+        dot_size = st.sidebar.slider("Dot Size", 5, 800, 800)
+        gap_size = st.sidebar.slider("Gap Size", 10, 50, 24)
+        canvas_size = st.sidebar.slider("Canvas Size", 70, 800, 228)
         shape = st.sidebar.selectbox("Dot Shape", ["circle", "square", "triangle", "pentagon", "hexagon", "diamond"])
         dpi = st.sidebar.selectbox("Resolution (DPI)", [100, 200, 300], index=2)
+        figsize_ratio = st.sidebar.slider("Figsize Ratio", 0.1, 10.0, 3.33)
 
-        self.create_dot_painting(dot_size, gap_size, canvas_size, shape, dpi)
+        image_format = st.sidebar.selectbox("Image Format", ["SVG", "JPEG"], index=0)
+
+        self.create_dot_painting(dot_size, gap_size, canvas_size, shape, dpi, figsize_ratio, image_format)
 
 if __name__ == "__main__":
     dot_painter = DotPainter()
