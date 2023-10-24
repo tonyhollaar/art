f86e116bea8cb16a9523b3f9d3b7caf1bd41dae8
tony hollaar <thollaar@gmail.com>
2023-10-23 21:59:40 -0400

    moved all color_options from matplotlib to .py

--- damien_hirst/color_options.py
+++ damien_hirst/color_options.py
@@ -1,39 +1,58 @@
 import matplotlib.cm as cm
+import streamlit as st
 
-class ColorOptions:
-    def __init__(self):
-        pass
 
-    @staticmethod
-    def get_pop_art_colors():
+def get_color_list(color_style, num_colors):
+    if color_style == "Pop Art":
         return ["#03BFAC", "#75DFCA", "#1DBACC", "#ED3192", "#087FBF"]
-
-    @staticmethod
-    def get_harmony_hues_colors():
+    elif color_style == "Harmony Hues":
         return ["#EFE0F3", "#325A94", "#EB9759", "#CF0A33", "#924265",
                 "#E8D05A", "#ED4569", "#3BAE47", "#7CB6A4", "#3C896E",
                 "#75A6D1", "#F94318", "#C791A8", "#F1F8F4", "#758B41",
                 "#896CB8", "#B1B519", "#026A4A", "#C9D306", "#2D2A65",
                 "#015574", "#B2C7B3", "#403C38", "#4C2F36", "#28A3D2",
                 "#2C2359", "#DAAFBB", "#E7ACA3"]
-
-    @staticmethod
-    def get_mono1_colors():
+    # Monochromatic 1
+    elif color_style == "Mono 1":
         return ["#0000FF", "#1111FF", "#2222FF", "#3333FF", "#4444FF",
                 "#5555FF", "#6666FF", "#7777FF", "#8888FF", "#9999FF"]
-
-    @staticmethod
-    def get_mono2_colors(base_color="#0000FF"):
-        # Define a list of monochromatic colors based on the selected base color
+    # Monochromatic 2
+    elif color_style == "Mono 2":
+        base_color = st.color_picker("Choose a Base Color", "#0000FF")
         monochromatic_colors = [base_color]
-        for i in range(1, 10):  # Generate 10 shades of the base color
+        for i in range(1, 10):
             shade = base_color.replace("#", f"#{i * 1:02X}")
             monochromatic_colors.append(shade)
         return monochromatic_colors
-
-    # Add similar methods for other color styles
-
-    @staticmethod
-    def get_colormap_colors(colormap_name, num_colors):
-        colormap = getattr(cm, colormap_name)
+    # Perceptually Uniform Sequential
+    elif color_style in ["Viridis", "Plasma", "Inferno", "Magma", "Cividis"]:
+        colormap = getattr(cm, color_style.lower())  # Correct the color_style name
+        return [colormap(x) for x in np.linspace(0, 1, 25)]
+    # Qualitative
+    elif color_style in ["Pastel1", "Pastel2", "Paired", "Accent", "Dark2", "Set1", "Set2", "Set3", "tab10", "tab20", "tab20b", "tab20c"]:
+        colormap = getattr(cm, color_style)
         return [colormap(x) for x in np.linspace(0, 1, num_colors)]
+    # Sequential
+    elif color_style in ['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds', 'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd',
+                         'RdPu', 'BuPu', 'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']:
+        colormap = getattr(cm, color_style)  # Correct the color_style name
+        return [colormap(x) for x in np.linspace(0, 1, num_colors)]
+    # Sequential2
+    elif color_style in ['binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink', 'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper']:
+        colormap = getattr(cm, color_style)  # Correct the color_style name
+        return [colormap(x) for x in np.linspace(0, 1, num_colors)]
+    # Cyclic
+    elif color_style in ['twilight', 'twilight_shifted', 'hsv']:
+        colormap = getattr(cm, color_style)  # Correct the color_style name
+        return [colormap(x) for x in np.linspace(0, 1, num_colors)]
+    # Diverging
+    elif color_style in ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']:
+        colormap = getattr(cm, color_style)  # Correct the color_style name
+        return [colormap(x) for x in np.linspace(0, 1, num_colors)]
+    # Miscellaneous
+    elif color_style in ['flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern', 'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet', 'turbo', 'nipy_spectral', 'gist_ncar']:
+        colormap = getattr(cm, color_style)  # Correct the color_style name
+        return [colormap(x) for x in np.linspace(0, 1, num_colors)]
+        colormap = getattr(cm, color_style)  # Correct the color_style name
+    else:
+        return []  # Return an empty list if the color style is not recognized
--- damien_hirst/streamlit_app.py
+++ damien_hirst/streamlit_app.py
@@ -6,6 +6,7 @@ import matplotlib.cm as cm
 from io import BytesIO
 from my_fonts import font_style, my_text_header, my_text_paragraph
 from barnsley_fern import BarnsleyFern
+from color_options import get_color_list
 
 st.set_page_config(
     page_title="Dotty",
@@ -46,7 +47,10 @@ class DotPainter:
             col1, col2, col3 = st.columns([1, 2, 1])
             with col2:
                 st.image('https://raw.githubusercontent.com/tonyhollaar/art/main/damien_hirst/images/dotty_logo.svg', width=250)  # logo
+
         my_text_header('CREATE YOUR SPOT PAINTING', my_font_family='Oswald')
+
+        # add some newlines for added space between header and dot painting
         vertical_spacer(2)
         self.color_list = []
         self.n = 1000  # Default value for Barnsley Fern Shape
@@ -163,16 +167,24 @@ class DotPainter:
             ax.scatter(x, y, c=colors, s=dot_size, marker=marker)
 
         if canvas_shape == "barnsley fern":
+            # load bar
+            my_bar = st.progress(0, text="Operation in progress. Please wait.")
+
             blf = BarnsleyFern()
             color_list = self.color_list
             x, y, colors = blf.draw_fern(n, color_list)
-
             fig, ax = plt.subplots(figsize=(5, 5))
             ax.axis('off')
 
             for i in range(n):
                 color = colors[i]
                 ax.scatter(x[i], y[i], s=1, c=color)
+
+                # load bar show progress to user
+                my_bar.progress((i / n), text=f"fern in progress: {int((i / n) * 100)+1}%")
+
+            # clear load bar
+            my_bar.empty()
 
         if image_format == "SVG":
             # Create an SVG image
@@ -186,7 +198,6 @@ class DotPainter:
             buffer = BytesIO()
             plt.savefig(buffer, format="jpeg", bbox_inches='tight', dpi=dpi)  # Use 'tight' to remove excess white space
             buffer.seek(0)
-
             st.image(buffer)
 
         with st.sidebar:
@@ -198,8 +209,7 @@ class DotPainter:
                         file_name=f"dot_painting.svg",
                         mime="image/svg+xml",
                         key="svg_download_button",
-                        use_container_width=True
-                    )
+                        use_container_width=True)
                 else:
                     st.download_button(
                         label=f"Download Image (JPEG)",
@@ -207,8 +217,7 @@ class DotPainter:
                         file_name=f"dot_painting.jpeg",
                         mime="image/jpeg",
                         key="jpeg_download_button",
-                        use_container_width=True
-                    )
+                        use_container_width=True)
 
     def run(self):
         """
@@ -218,99 +227,33 @@ class DotPainter:
         """
         with st.sidebar:
             my_text_header('palette', my_font_family='Oswald')
-
-            # =============================================================================
-            # COLOR STYLES
-            # =============================================================================
             col1, col2, col3, col4 = st.columns([1, 5, 5, 1])
-
             with col2:
-                # Create a selectbox in the sidebar to choose the color style
                 color_style = st.selectbox("Select Color Style:",
-                                           ["Pop Art", "Harmony Hues", "Mono 1", "Mono 2", "Viridis", "Plasma",
-                                            "Inferno",
-                                            "Magma", "Cividis", "Pastel1", "Pastel2", "Paired", "Accent", "Dark2",
-                                            "Set1",
-                                            "Set2", "Set3", "tab10", "tab20", "tab20b", "tab20c"])
+                                           ["Pop Art", "Harmony Hues",
+                                            "Mono 1", "Mono 2",
+                                            "Viridis", "Plasma", "Inferno", "Magma", "Cividis",
+                                            "Pastel1", "Pastel2", "Paired", "Accent", "Dark2",
+                                            "Set1", "Set2", "Set3", "tab10", "tab20", "tab20b", "tab20c",
+                                            'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds', 'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu', 'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn',
+                                            'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink', 'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper',
+                                            'twilight', 'twilight_shifted', 'hsv',
+                                            'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic',
+                                            'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern', 'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet', 'turbo', 'nipy_spectral', 'gist_ncar'])
                 dot_size = st.slider("Dot Size", 5, 800, 800)
                 gap_size = st.slider("Gap Size", 10, 50, 24)
-                n = st.slider('Enter the number of points in the Fern:', 100, 10000, 1000)  # Barnsley Fern Shape
+                num_colors = st.slider("Number of Colors", 1, 25, 10)
 
-            # Set color_list based on the user's selection
-            if color_style == "Pop Art":
-                self.color_list = ["#03BFAC", "#75DFCA", "#1DBACC", "#ED3192", "#087FBF"]
-
-            elif color_style == "Harmony Hues":
-                self.color_list = ["#EFE0F3", "#325A94", "#EB9759", "#CF0A33", "#924265",
-                                   "#E8D05A", "#ED4569", "#3BAE47", "#7CB6A4", "#3C896E",
-                                   "#75A6D1", "#F94318", "#C791A8", "#F1F8F4", "#758B41",
-                                   "#896CB8", "#B1B519", "#026A4A", "#C9D306", "#2D2A65",
-                                   "#015574", "#B2C7B3", "#403C38", "#4C2F36", "#28A3D2",
-                                   "#2C2359", "#DAAFBB", "#E7ACA3"]
-
-            elif color_style == "Mono 1":
-                self.color_list = ["#0000FF", "#1111FF", "#2222FF", "#3333FF", "#4444FF",
-                                   "#5555FF", "#6666FF", "#7777FF", "#8888FF", "#9999FF"]
-
-            elif color_style == "Mono 2":
-                # Create a color picker to select the base color
-                with col2:
-                    base_color = st.color_picker("Choose a Base Color", "#0000FF")
-
-                # Define a list of monochromatic colors based on the selected base color
-                monochromatic_colors = [base_color]
-                for i in range(1, 10):  # Generate 10 shades of the base color
-                    shade = base_color.replace("#", f"#{i * 1: 02X}")
-                    monochromatic_colors.append(shade)
-
-                self.color_list = monochromatic_colors
-
-            ####################################################################################
-            # Perceptually Uniform Sequential
-            ####################################################################################
-            elif color_style == "Viridis":
-                # Use the 'viridis' color map from Matplotlib
-                self.color_list = [cm.viridis(x) for x in np.linspace(0, 1, 25)]
-
-            elif color_style == "Plasma":
-                # Use the 'viridis' color map from Matplotlib
-                self.color_list = [cm.plasma(x) for x in np.linspace(0, 1, 25)]
-
-            elif color_style == "Inferno":
-                # Use the 'viridis' color map from Matplotlib
-                self.color_list = [cm.inferno(x) for x in np.linspace(0, 1, 25)]
-
-            elif color_style == "Magma":
-                # Use the 'viridis' color map from Matplotlib
-                self.color_list = [cm.magma(x) for x in np.linspace(0, 1, 25)]
-
-            elif color_style == "Cividis":
-                # Use the 'viridis' color map from Matplotlib
-                self.color_list = [cm.cividis(x) for x in np.linspace(0, 1, 25)]
-
-            ####################################################################################
-            # Qualitative colormaps
-            ####################################################################################
-            elif color_style in ["Pastel1", "Pastel2", "Paired", "Accent", "Dark2", "Set1", "Set2", "Set3", "tab10",
-                                 "tab20", "tab20b", "tab20c"]:
-
-                # Define the number of colors you want from the selected colormap
-                with col3:
-                    num_colors = st.slider("Number of Colors", 1, 25, 10)
-
-                # Extract the colors by creating a list of evenly spaced values between 0 and 1
-                colormap = getattr(cm, color_style)
-                style_colors = [colormap(x) for x in np.linspace(0, 1, num_colors)]
-
-                # Assign the extracted colors to self.color_list
-                self.color_list = style_colors
-
+                # Call the get_color_list function to set self.color_list based on the selected color_style
+                self.color_list = get_color_list(color_style, num_colors)
             with col3:
                 shape = st.selectbox("Dot Shape", ["circle", "square", "triangle", "pentagon", "hexagon", "diamond"])
-                canvas_shape = st.selectbox("Shape", ["square", "circle1", "circle2", "golden ratio", "barnsley fern"],
-                                            index=0)
+                canvas_shape = st.selectbox("Shape", ["square", "circle1", "circle2", "golden ratio", "barnsley fern"], index=0)
                 canvas_size = st.slider("Canvas Size", 70, 800, 228)
                 figsize_ratio = st.slider("Figsize Ratio", 0.1, 10.0, 3.33)
+            with col2:
+                if canvas_shape == "barnsley fern":
+                    n = st.slider('Number of points in the Fern', 100, 10000, 1000)  # Barnsley Fern Shape
 
             # =============================================================================
             # Download
